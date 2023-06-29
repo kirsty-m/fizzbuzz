237756803db766ce491eb4aa321bd0f4862725d0
Kirsty <kirstmcbride@gmail.com>
2023-06-29 10:54:51 +0100

    fizzbuzz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [number, setNumber, ] = useState(0);
+  const [fizz, setFizz] = useState(0);
+
+  
+
+  useEffect( () => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      setFizz("fizzbuzz")
+    } else if (number % 3 === 0) {
+      setFizz("fizz")
+    } else if (number % 5 === 0) {
+      setFizz("buzz")
+    } else {
+      setFizz(number);
+
+    } 
+  });
+
+  const handleInc = () => {
+    setNumber(number + 1);
+
+  };
+
+  const handleDec = () => {
+    setNumber(number - 1);
+
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>FizzBuzz</h1>
+    <button value={number} onClick={handleInc}> + </button>
+    <button value={number} onClick={handleDec}> - </button>
+
+    <h2>Number is {fizz}</h2>
+    </>
   );
 }
 
